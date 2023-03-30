64a6d0ff2579dcb6ba7de864185b36c7056ca6d3
Keavon Chambers <keavon@keavon.com>
2023-03-29 21:35:06 -0700

    Working slice expansion without correct alignment

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,27 @@
 import "./style.scss";
 
+const TAU = Math.PI * 2;
 const CATEGORIES = 12;
 const TOPICS_PER_CATEGORY = 6;
 const TOTAL_TOPICS = CATEGORIES * TOPICS_PER_CATEGORY;
+const CATEGORY_ANGLE = TAU / CATEGORIES;
+const SLICE_ANGLE = TAU / TOTAL_TOPICS;
+const CONFLATION_FIX_ANGLE = 0.2 / TAU;
 const SLICE_RADIUS = 120; // Slightly more than 100 so clipping can keep it circular
-const CONFLATION_FIX_ANGLE = 0.2 / (Math.PI * 2);
+
+let testAngle = 30;
+const testCategory = 6;
+
+window.addEventListener("keydown", (e) => {
+	if (e.key === "ArrowUp") {
+		testAngle += 0.5;
+		updateSlices(testCategory, (testAngle / 360) * TAU);
+	}
+	if (e.key === "ArrowDown") {
+		testAngle -= 0.5;
+		updateSlices(testCategory, (testAngle / 360) * TAU);
+	}
+});
 
 function init(): void {
 	const slices = document.querySelector("[data-slices]") || undefined;
@@ -18,20 +35,46 @@ function init(): void {
 		slices.appendChild(polygon);
 	}
 
-	updateSlices();
+	updateSlices(0, TAU / CATEGORIES);
 }
 
-function updateSlices(): void {
+function updateSlices(expandedCategory: number, expandedCategoryAngle: number): void {
+	// Get all the slice polygons
 	const slices = (Array.from(document.querySelectorAll("[data-slices] [data-slice]")) || undefined) as SVGPathElement[] | undefined;
 	if (!slices) return;
 
+	// Zero-indexed. Start is inclusive, stop is exclusive: [start, stop)
+	// Example: 0, 6 which corresponds to the first six slices (0 through 5)
+	// Example: 6, 12 which corresponds to the second six slices (6 through 11)
+	const expandedRangeStart = expandedCategory * TOPICS_PER_CATEGORY;
+	const expandedRangeStop = (expandedCategory + 1) * TOPICS_PER_CATEGORY;
+
+	// Angle of expanded and shrunk slices
+	const expandedSliceAngle = (expandedCategoryAngle / CATEGORY_ANGLE) * SLICE_ANGLE;
+	const shrunkSliceAngle = (TAU - expandedSliceAngle * TOPICS_PER_CATEGORY) / (TOTAL_TOPICS - TOPICS_PER_CATEGORY);
+
+	// Update the angles of each slice polygon
 	slices.forEach((slice, index) => {
 		const conflationFix = index === slices.length - 1 ? 0 : CONFLATION_FIX_ANGLE;
 
-		const sliceAngle = (Math.PI * 2) / TOTAL_TOPICS;
-		const startAngle = sliceAngle * index;
-		const stopAngle = sliceAngle * (index + 1) + conflationFix;
+		// Pick the start and stop angle for this slice
+		let startAngle;
+		let stopAngle;
+		if (index < expandedRangeStart) {
+			// This slice comes before the expanded category
+			startAngle = index * shrunkSliceAngle;
+			stopAngle = startAngle + shrunkSliceAngle + conflationFix;
+		} else if (index < expandedRangeStop) {
+			// This slice is within the expanded category
+			startAngle = expandedRangeStart * shrunkSliceAngle + (index - expandedRangeStart) * expandedSliceAngle;
+			stopAngle = startAngle + expandedSliceAngle + conflationFix;
+		} else {
+			// This slice comes after the expanded category
+			startAngle = expandedRangeStart * shrunkSliceAngle + (expandedRangeStop - expandedRangeStart) * expandedSliceAngle + (index - expandedRangeStop) * shrunkSliceAngle;
+			stopAngle = startAngle + shrunkSliceAngle + conflationFix;
+		}
 
+		// Write the vertex angles to the polygon for this slice
 		const startX = -Math.sin(startAngle) * SLICE_RADIUS;
 		const startY = -Math.cos(startAngle) * SLICE_RADIUS;
 		const stopX = -Math.sin(stopAngle) * SLICE_RADIUS;
